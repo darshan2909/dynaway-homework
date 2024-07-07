1ef024e42e0e865ca43b0ff298031f14e3fe1b04
Darshan <darshanmdad@gmail.com>
2024-07-07 20:45:31 +0530

    Task #1: Fixed asset data displaying in home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,6 +14,9 @@ export class HomePage {
 
   ionViewWillEnter(): void {
     this.assets = []
-    this.assetService.getAll().subscribe(assets => this.assets = assets)
+    this.assetService.getAll()
+    .subscribe((assets:any) => {
+      this.assets = assets.data
+    })
   }
 }
